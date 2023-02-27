e50e1970fc9121d89f3e3029be9205e8c162b2fc
leemj0948 <ljo094822@naver.com>
2023-02-27 12:02:38 +0900

    refeactor: refeactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,28 @@
 import { useEffect, useState } from "react";
 import AppRouter from "./Router";
-import {authService, isAuthChange} from 'fbase';
+import { authService, isAuthChange } from "fbase";
 
 function App() {
-  const auth = authService;
-  const [init,setInit] = useState(false);
-  const [isLoggedIn,setIsLoggedIn] = useState(false);
-  const [userObj,setUserObj]=useState(null);  // 추후 전역변수로 관리할것 
-  const isAuth = isAuthChange(auth,(user)=>{
-    if(user){
-      setIsLoggedIn(true)
-      setUserObj(user);
-    }else{
-      setIsLoggedIn(false)
-    }
-    setInit(true);
-  });
-  useEffect(isAuth,[])
+    const auth = authService;
+    const [init, setInit] = useState(false);
+    const [userObj, setUserObj] = useState(null); // 추후 전역변수로 관리할것
+    const isAuth = isAuthChange(auth, (user) => {
+        if (user) {
+            setUserObj(user);
+        }
+        setInit(true);
+    });
+    useEffect(isAuth, []);
 
-  return (
-    <>
-     {init? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>:'Initializeing'}
-    </>
-   
-  
-  );
+    return (
+        <>
+            {init ? (
+                <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+            ) : (
+                "Initializeing"
+            )}
+        </>
+    );
 }
 
 export default App;
